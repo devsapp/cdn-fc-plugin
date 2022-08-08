e73d0ae730ddbd977ddb5bbbb096aac56aa72c24
Fitz <fantasy084@icloud.com>
2022-08-08 09:56:05 +0800

    feat: 针对有多个自定义域名的服务名和函数匹配的情况提出解决方案 + 自定义域名的路由规则中存在一条路由的服务名，函数名与目标服务名和函数名相同且路由路径为`/*`直接设置为主源 + 当自定义域名的路由规则的所有服务名都为目标服务名时，也可作为源，默认为备源，如所有源都为备源时，最新的一个源会改为主源 + 权重按自定义域名由新到旧排序，新的自定义域名权重越大

--- src/index.js
+++ src/index.js
@@ -39,33 +39,110 @@ module.exports = async function index(inputs, args) {
     config.endpoint = `${credentials.AccountID}.${region}.fc.aliyuncs.com`;
 
     const client = new FC_Open20210406(config);
-    let listCustomDomainsRequest = new ListCustomDomainsRequest({prefix: `${functionName}.${serviceName}`, limit: 1});
+    let listCustomDomainsRequest = new ListCustomDomainsRequest({limit: 100});
 
-    const customDomains = (await client.listCustomDomains(listCustomDomainsRequest)).body.customDomains;
+    const domains = [];
+    let newNextToken = '';
+    do {
+        listCustomDomainsRequest.nextToken = newNextToken;
+        const {customDomains, nextToken} = (await client.listCustomDomains(listCustomDomainsRequest)).body;
+        newNextToken = nextToken;
+        if (lodash.isEmpty(customDomains)) {
+            break;
+        }
+        customDomains.forEach(i => {
+            const routeConfig = i.routeConfig;
+            if (!lodash.isEmpty(routeConfig) && !lodash.isEmpty(routeConfig.routes)) {
+                const routes = routeConfig.routes;
+                let hasOther = false;
+                for (let j = 0; j < routes.length; j++) {
+                    const route = routes[j];
+                    const rServiceName = route.serviceName;
+                    const rFunctionName = route.functionName;
+                    if (rServiceName == serviceName) {
+                        if (rFunctionName == functionName && route.path == "/*") {
+                            domains.push({
+                                path: '/*',
+                                domainName: i.domainName
+                            });
+                            break;
+                        }
 
-    if (lodash.isEmpty(customDomains)) {
+                        // 如果该自定义域名都属于目标Service，也符合条件
+                        if (!hasOther && j == routes.length -1) {
+                            domains.push({
+                                path: 'other',
+                                domainName: i.domainName
+                            });
+                        }
+                    } else {
+                        hasOther = true;
+                    }
+                }
+
+
+            }
+        })
+    } while (newNextToken);
+
+
+    if (domains.length <= 0) {
         throw new CatchableError(`cannot find the service<${serviceName}> function <${functionName}> 's domainName `);
     }
 
-    const customDomain = customDomains[0];
 
-    const domainName = customDomain.domainName;
-    if (lodash.isEmpty(domainName)) {
-        throw new CatchableError(`cannot find the service<${serviceName}> function <${functionName}> 's domainName `);
+    let sources = [];
+
+    // 如果只有只有一个自定义域名匹配，直接设置为主源
+    if (domains.length == 1) {
+        const domain = domains[0];
+        sources.push({
+            type: 'fc_domain',
+            content: domain.domainName,
+            priority: 20,
+            weight: 100
+        });
+    } else {
+        let mainWeight = 100;
+        let otherWeight = 100;
+        let mainCounter = 0;
+        sources = domains.map((value, index) => {
+            // path为'/*' 的为主源
+            if (value.path == "/*") {
+                // 权重最小值为10
+                const weight = mainWeight == 10 ? 10 : mainWeight--;
+                mainCounter++;
+                return {
+                    type: 'fc_domain',
+                    content: value.domainName,
+                    priority: 20,
+                    weight
+                };
+            } else {
+                // 权重最小值为10
+                const weight = otherWeight == 10 ? 10 : otherWeight--;
+
+                return {
+                    type: 'fc_domain',
+                    content: value.domainName,
+                    priority: 30,
+                    weight
+                };
+            }
+        });
+
+        // 如果/*作为主源，则将第一备源改为主源
+        if (mainCounter == 0) {
+            sources[0].priority = 20;
+        }
     }
-
-    SPINNER_VM.info(`get service<${serviceName}> function <${functionName}> 's domainName<${domainName}> successed!`)
+    SPINNER_VM.info(`get service<${serviceName}> function <${functionName}> 's domainNames:\n${sources.map(i => i.content + " : " + (i.priority == 20 ? "主源" : "备源")).join('\n')}`)
 
     SPINNER_VM.stop();
 
     return lodash.merge(inputs, {
         props: {
-            sources: [
-                {
-                    type: 'fc_domain',
-                    content: domainName
-                }
-            ]
+            sources
         },
     });
 };
